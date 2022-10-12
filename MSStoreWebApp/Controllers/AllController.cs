155c4a532ff910a6f177952277aca5fb485c9483
Lacromisss <user.email>
2022-10-12 15:09:53 +0400

    Siuuu

---
+++ MSStoreWebApp/Controllers/AllController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MSStoreWebApp.Controllers
+{
+    public class AllController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        } 
+        public IActionResult Smart()
+        {
+            return View();
+        } 
+        public IActionResult Girl()
+        {
+            return View();
+        }
+    }
+}

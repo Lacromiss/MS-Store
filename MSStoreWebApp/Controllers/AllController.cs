39e2b13a761ec555fc711c151cb0a54bdfd70c9d
Lacromisss <user.email>
2022-10-16 16:40:32 +0400

    done

--- MSStoreWebApp/Controllers/AllController.cs
+++ MSStoreWebApp/Controllers/AllController.cs
@@ -1,11 +1,23 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MSStoreWebApp.Dal;
+using MSStoreWebApp.Migrations;
+using MSStoreWebApp.Models;
+using System.Collections.Generic;
 
 namespace MSStoreWebApp.Controllers
 {
     public class AllController : Controller
     {
+        private AppDbContext _context;
+
+        public AllController(AppDbContext context)
+        {
+            _context = context;
+
+        }
         public IActionResult Index()
         {
+
             return View();
         } 
         public IActionResult Smart()
